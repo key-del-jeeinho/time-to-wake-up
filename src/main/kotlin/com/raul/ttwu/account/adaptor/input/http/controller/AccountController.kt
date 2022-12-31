29f70e79753e740ed64fd6db73b109450ffcd08e
devel <develop.raul@gmail.com>
2023-01-01 02:02:58 +0900

    fix: account controller to allow account controller test to pass
    delegate business logic to account use case

--- src/main/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountController.kt
+++ src/main/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountController.kt
@@ -3,12 +3,17 @@ package com.raul.ttwu.account.adaptor.input.http.controller
 import com.raul.ttwu.account.adaptor.input.http.data.request.CreateAccountRequest
 import com.raul.ttwu.account.adaptor.input.http.data.response.AccountDataResponse
 import com.raul.ttwu.account.adaptor.input.http.data.response.CreateAccountResponse
+import com.raul.ttwu.account.adaptor.input.http.extension.toDomain
+import com.raul.ttwu.account.application.port.input.AccountUseCase
 import org.springframework.http.ResponseEntity
 
-class AccountController {
+class AccountController(
+    private val accountUseCase: AccountUseCase
+) {
     fun createAccount(request: CreateAccountRequest): ResponseEntity<CreateAccountResponse> {
-        return AccountDataResponse(id = request.id, name = request.name)
-            .let { data -> CreateAccountResponse(0, data) }
+        val account = accountUseCase.createAccount(request.toDomain())
+        return AccountDataResponse(id = account.id, name = account.name)
+            .let { data -> CreateAccountResponse(account.idx, data) }
             .let { response -> ResponseEntity.ok(response) }
     }
 }
