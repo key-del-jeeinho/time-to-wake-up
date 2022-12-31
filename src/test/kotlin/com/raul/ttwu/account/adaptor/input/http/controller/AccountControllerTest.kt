11e636a019ca90fa3b325294a63e9b929755d4f6
devel <develop.raul@gmail.com>
2023-01-01 02:08:13 +0900

    fix: account controller test
    take mock and target objects out of given scope

--- src/test/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountControllerTest.kt
+++ src/test/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountControllerTest.kt
@@ -12,10 +12,9 @@ import io.mockk.verify
 import org.springframework.http.HttpStatus
 
 class AccountControllerTest: BehaviorSpec({
+    val accountUseCase = mockk<AccountUseCase>()
+    val controller = AccountController(accountUseCase)
     given("create account request") {
-        val accountUseCase = mockk<AccountUseCase>()
-        val controller = AccountController(accountUseCase)
-
         val request = CreateAccountRequest("홍길동", "id_asdmxiv", "pw_vsiddf")
         val createAccount = CreateAccount(request.name, request.id, request.password)
         val account = Account(0, "_홍길동", "_id_asdmxiv", "_epw_vsiddf")
