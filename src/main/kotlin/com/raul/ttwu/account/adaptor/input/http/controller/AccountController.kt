cb4e6bdebed77cf4b9573d627844a66abef97f76
devel <develop.raul@gmail.com>
2023-01-01 01:17:47 +0900

    fix: account controller to allow account controller test to pass
    remove todo mark and implement return logic

--- src/main/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountController.kt
+++ src/main/kotlin/com/raul/ttwu/account/adaptor/input/http/controller/AccountController.kt
@@ -1,11 +1,14 @@
 package com.raul.ttwu.account.adaptor.input.http.controller
 
 import com.raul.ttwu.account.adaptor.input.http.data.request.CreateAccountRequest
+import com.raul.ttwu.account.adaptor.input.http.data.response.AccountDataResponse
 import com.raul.ttwu.account.adaptor.input.http.data.response.CreateAccountResponse
 import org.springframework.http.ResponseEntity
 
 class AccountController {
     fun createAccount(request: CreateAccountRequest): ResponseEntity<CreateAccountResponse> {
-        TODO("Not yet implemented")
+        return AccountDataResponse(id = request.id, name = request.name)
+            .let { data -> CreateAccountResponse(0, data) }
+            .let { response -> ResponseEntity.ok(response) }
     }
 }
