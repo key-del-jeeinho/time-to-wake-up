23ab6d95969ee2734d3774d3c336b80f60f80f10
devel <develop.raul@gmail.com>
2022-12-31 15:11:01 +0900

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "time-to-wake-up"
---
+++ src/main/kotlin/com/raul/ttwu/TimeToWakeUpApplication.kt
@@ -0,0 +1,11 @@
+package com.raul.ttwu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TimeToWakeUpApplication
+
+fun main(args: Array<String>) {
+    runApplication<TimeToWakeUpApplication>(*args)
+}
