21df752d6c566b1a78e9e09f1061aa964498f643
devel <develop.raul@gmail.com>
2023-01-01 05:08:44 +0900

    fix: migrate integration tests to kotest spec

--- src/test/kotlin/com/raul/ttwu/IntegrationTestSpec.kt
+++ src/test/kotlin/com/raul/ttwu/IntegrationTestSpec.kt
@@ -1,8 +1,8 @@
 package com.raul.ttwu
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.core.spec.style.BehaviorSpec
 import org.junit.jupiter.api.Disabled
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
@@ -12,9 +12,8 @@ import org.springframework.transaction.annotation.Transactional
 @Disabled
 @Transactional
 @AutoConfigureMockMvc
-class IntegrationTestBase {
-    @Autowired
-    protected lateinit var mvc: MockMvc
-    @Autowired
-    protected lateinit var objectMapper: ObjectMapper
-}
+class IntegrationTestSpec(
+    mvc: MockMvc,
+    objectMapper: ObjectMapper,
+    body: BehaviorSpec.() -> Unit = {}
+): BehaviorSpec(body)
--- src/test/kotlin/com/raul/ttwu/account/AccountIntegrationTest.kt
+++ src/test/kotlin/com/raul/ttwu/account/AccountIntegrationTest.kt
@@ -1,30 +1,20 @@
 package com.raul.ttwu.account
 
-import com.raul.ttwu.IntegrationTestBase
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.raul.ttwu.IntegrationTestSpec
 import com.raul.ttwu.account.adaptor.input.http.data.request.CreateAccountRequest
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.notNullValue
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
-import java.util.stream.Stream
 
-class AccountIntegrationTest: IntegrationTestBase() {
-    companion object {
-        @JvmStatic
-        fun accountData(): Stream<Arguments> = Stream.of(
-            Arguments.arguments("홍길동", "et_asdmxiv", "pw_vsiddf"),
-            Arguments.arguments("김홍도", "et_shnbdf", "pw_usd"),
-            Arguments.arguments("김삿갓", "et_ug", "pw_pasdsfgms"),
-            Arguments.arguments("로버트.C.마틴", "et_s0ijfav.sad", "pw_wgefjs"),
-            Arguments.arguments("아무개", "et_mwrmj", "pw_ofhrem"),
-        )
-    }
+class AccountIntegrationTest(
+    mvc: MockMvc,
+    objectMapper: ObjectMapper,
+): IntegrationTestSpec(mvc, objectMapper, {
 
     /*
     feature: create account
@@ -32,24 +22,26 @@ class AccountIntegrationTest: IntegrationTestBase() {
     #2 failure (id already exists)
     #4 failure (password is not valid)
      */
-    @ParameterizedTest
-    @DisplayName("create account #1 success")
-    @MethodSource("accountData")
-    fun `create account #1 success`(name: String, id: String, password: String) {
-        //given
-        val request = CreateAccountRequest(name, id, password)
-        //when
-        val resultAction = mvc.perform(post("/api/v1/accounts")
+    given("create account request (#1 success)") {
+        val request = CreateAccountRequest("홍길동", "id_asdmxiv", "pw_vsiddf")
+        `when`("is received") {
+            val resultAction = mvc.perform(post("/api/v1/accounts")
                 .contentType(APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request))
                 .accept(APPLICATION_JSON))
                 .andDo{ print(it.response.contentAsString) }
-        //then
-        resultAction
-            .andExpect(status().isOk)
-            .andExpect(jsonPath("accountIdx", `is`(notNullValue())))
-            .andExpect(jsonPath("data.name", `is`(name)))
-            .andExpect(jsonPath("data.id", `is`(id)))
+
+            then("response status should be OK") {
+                resultAction.andExpect(status().isOk)
+            }
+
+            then("response body should be in context of create account") {
+                resultAction
+                    .andExpect(jsonPath("accountIdx", `is`(notNullValue())))
+                    .andExpect(jsonPath("data.name", `is`(request.name)))
+                    .andExpect(jsonPath("data.id", `is`(request.id)))
+            }
+        }
     }
 
     /*
@@ -79,4 +71,4 @@ class AccountIntegrationTest: IntegrationTestBase() {
     #3 failure (not deletable data)
     #4 failure (permission denied)
      */
-}
+})
